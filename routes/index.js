f4b794d39e21bc8e7d7672e6def934c93df1fcc1
dabutvin <dabutvin@microsoft.com>
2016-10-10 22:58:18 -0700

    slidey thing

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Elise :)' });
+  res.render('index', { title: 'Elise :)', cache: false });
 });
 
 module.exports = router;
