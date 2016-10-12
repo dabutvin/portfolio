97af113315b2cc72f7acb8e4d0c2530f3685a7bb
dabutvin <dabutvin@microsoft.com>
2016-10-11 19:23:26 -0700

    debounce and preload slidey

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -1,5 +1,22 @@
-window.onscroll = function() {
-    if (window.scrollY > 1000) {
+window.onscroll = debounce(function() {
+    if (window.scrollY < 1050) {
+        document.body.classList.remove('scrolled-2')
+    } else if (window.scrollY > 1050) {
         document.body.classList.add('scrolled-2')
     }
+}, 100);
+
+function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+        var context = this, args = arguments;
+        var later = function() {
+            timeout = null;
+            if (!immediate) func.apply(context, args);
+        };
+        var callNow = immediate && !timeout;
+        clearTimeout(timeout);
+        timeout = setTimeout(later, wait);
+        if (callNow) func.apply(context, args);
+    };
 };
